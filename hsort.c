c211933b832c573a459ee137ae21a10661d01b25
cat4rsys <aleksejkornilov573@gmail.com>
2024-12-14 19:06:53 +0300

    updated code

--- hsort.c
+++ hsort.c
@@ -1,36 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include<time.h>
+#include<stdint.h>
 char** array = NULL;
-static int swapCounter = 0;
+size_t swapCounter = 0;
 
 void swap(char** a, char** b) {
+	swapCounter++;
 	char* c = *a;
 	*a = *b;
 	*b = c;
-	swapCounter++;
 }
 
 #define PARENT(e) (((e)-1)/2)
 #define LEFT(e) (((e)+1)*2-1)
 #define RIGHT(e) (((e)+1)*2)
 
-int heapify(char** arr, int i, int sz) {
-	if ( sz <= 1) return 0;
-	if ( i == 0 ) return heapify(arr, sz-2, sz-1);
-	if ( strcmp(arr[i], arr[PARENT(i)]) > 0 )
-		swap(&arr[i], &arr[PARENT(i)]);
-	return heapify(arr, PARENT(i), sz);
+int heapify(char** arr, int i, int s, int sz) {
+    if (sz <= s ) return 0;
+
+    if ( i == 0 || strcmp(arr[i], arr[PARENT(i)]) >0 ) return heapify(arr, s+1, s+1, sz);
+
+    swap(&arr[i], &arr[PARENT(i)]);
+    return heapify(arr, PARENT(i), s, sz);
 }
+
 
 int heapdown(char** arr, int i, int sz) {
 	char *x = arr[i];
 	char* min = x;
 	int mini = i;
-	if ( LEFT(i) < sz && strcmp(arr[LEFT(i)], x) > 0 )
+	if ( LEFT(i) < sz && strcmp(arr[LEFT(i)], x) < 0 )
 		min = arr[LEFT(i)], mini = LEFT(i);
-	if ( RIGHT(i) < sz && strcmp(arr[RIGHT(i)], min) > 0  )
+	if ( RIGHT(i) < sz && strcmp(arr[RIGHT(i)], min) < 0  ) 
 		min = arr[RIGHT(i)], mini = RIGHT(i);
 	if ( i != mini) {
 		swap(&arr[i], &arr[mini]);
@@ -48,25 +51,31 @@ int sortify(char** arr, int sz) {
 }
 
 int heapsort(char** arr, int sz) {
-	return heapify(arr, sz-1, sz) || sortify(arr, sz);
+	return heapify(arr, 1, 1, sz) || sortify(arr, sz);
 }
 
+#define SEC_TO_US(sec) ((sec)*1000000)
+#define NS_TO_US(ns)   ((ns)/1000)
+
+uint64_t micros()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    uint64_t us = SEC_TO_US((uint64_t)ts.tv_sec) + NS_TO_US((uint64_t)ts.tv_nsec);
+    return us;
+}
 
 int main() {
 	size_t cap = 1024;
 	array = calloc(cap, sizeof(char*));
 	char buf[4096];
-	int i;
+	int i = 0;
 	while (fgets(buf, sizeof(buf), stdin)) {
 		if ( i >= cap) array = realloc(array, sizeof(char*)*(cap*=2));
 		array[i++] = strdup(buf);
 	}
-	/*fprintf(stderr, "Sorting....\n");*/
+	uint64_t timeStart = micros();
 	heapsort(array, i);
-	/*fprintf(stderr, "Heapified\n");
-	for ( int j = 0; j < i; j++ )
-		fputs(array[j], stdout);
-	free(array);*/
-
-	printf("Total amount of swaps: %d\nTotal amount of lines: %d\n", swapCounter, i);
+	printf("%d %zu %zu", i, swapCounter, micros() - timeStart);
+	free(array);
 }
